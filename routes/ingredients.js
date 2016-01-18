5b3d95c47797ba556658202dfdf48754bfb43720
Mauricio Andrades <mod.and.op@gmail.com>
2016-01-17 18:24:49 -0800

    snapshot, workin, filtered response object

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -7,8 +7,6 @@ var axios = require('axios');
 require('dotenv').load();
 var router = express.Router();
 
-
-debugger;
 router.get('/', function(req, res) {
     var qString = qs.stringify(req.query);
     var ApiEndpoint = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?";
@@ -17,55 +15,37 @@ router.get('/', function(req, res) {
     var searchQ = ApiEndpoint + qString + number;
 
     axios.get('https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients', {
-        url: '/ingredients',
-        method: 'get',
-        headers: {
-            "X-Mashape-Key": process.env.APIKEY
-        },
-        params: {
-            ingredients: req.query.ingredients,
-        },
-        timeout: 1000,
-        withCredentials: false,
-        responseType: 'json',
-        xsrfCookieName: 'XSRF-TOKEN',
-        xsrfHeaderName: 'X-XSRF-TOKEN',
-        // transformResponse: [function (data) {
-        //     return JSON.stringify(data);
-        //     // return data;
-        //   }],
-    })/*.then(function(data) {
-        console.log('--------------------hit');
-        var ids = [];
-        ids = ids.map(function(index, elem) {
-            data.data[i].id;
+            url: '/ingredients',
+            method: 'get',
+            headers: {
+                "X-Mashape-Key": process.env.APIKEY
+            },
+            params: {
+                ingredients: req.query.ingredients,
+            },
+            timeout: 1000,
+            withCredentials: false,
+            responseType: 'json',
+            xsrfCookieName: 'XSRF-TOKEN',
+            xsrfHeaderName: 'X-XSRF-TOKEN',
         })
-        for (var i = 0; i < ids.length; i++) {
-            console.log(ids[i]);
-        }
-        return data;
-        // fs.writeFile('./data.json', JSON.stringify(data.data, null, 2), 'utf-8');
-    })*/
-    .then(function(data) {
-        // console.log(data.data);
-        console.log(data.data[0].id);
-        // console.log(data.status);
-        // console.log(data.statusText);
-        // console.log(data.headers);
-        // console.log(data.config);
-        res.json(data);
-    })
-    .catch(function (response) {
-      if (response instanceof Error) {
-        console.log('Error', response.message);
-      } else {
-          res.send('idk');
-        // console.log(response.data);
-        // console.log(response.status);
-        // console.log(response.headers);
-        // console.log(response.config);
-      }
-    });
+        .then(function(data) {
+            var arr = data.data;
+            for (var i = 0; i < arr.length; i++) {1
+
+                res.title = arr[i].title,
+                res.id = arr[i].id,
+                res.image = arr[i].image
+            };
+        res.send(data.data);
+        })
+        .catch(function(response) {
+            if (response instanceof Error) {
+                console.log('Error', response.message);
+            } else {
+                res.json('idk');
+            }
+        });
 });
 
 module.exports = router;
