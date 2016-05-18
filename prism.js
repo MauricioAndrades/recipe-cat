f633e9f32e227b500b395eefc24c6c0405f02ded
Mauricio Andrades <mod.and.op@gmail.com>
2016-05-18 02:41:05 -0700

    added local font-awesome and prism

---
+++ prism.js
@@ -0,0 +1,7 @@
+var Prism = require('prismjs');
+
+// The code snippet you want to highlight, as a string
+var code = "var data = 1;";
+
+// Returns a highlighted HTML string
+var html = Prism.highlight(code, Prism.languages.javascript);
